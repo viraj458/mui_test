e00031fe2cd98e1174f9920486042493ccbc235a
Viraj <virajmalaka94@gmail.com>
2023-02-06 12:54:44 +0530

    customized button component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { Button, Typography } from '@mui/material';
+import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
 import React from 'react';
+import { margin } from '@mui/system';
+import styled from '@emotion/styled';
 
 function App() {
+
+  const BlueButton = styled(Button)({
+    backgroundColor: "skyblue",
+    color: "black",
+    margin: 5,
+    "&:hover":{
+      backgroundColor: "lightblue"
+    },
+    "&:disabled":{
+      backgroundColor:"grey",
+      color:"white"
+    }
+  })
+
   return (
     <div>
-      Hello
+      <Button variant="text">Text</Button>
+      <Button variant="contained" color='secondary' startIcon={<AccessibilityNewIcon/>}>Contained</Button>
+      <Button variant="outlined" disableElevation>Outlined</Button>
+      <Typography variant="h1">
+      h1. Heading
+      </Typography>
+      <BlueButton>the</BlueButton>
+      <BlueButton disabled>the</BlueButton>
+
     </div>
   );
 }
